235eb622f916ebf25eb183e102e2a53b2e3dd60d
MrChaker <mr.chakerw@gmail.com>
2022-06-29 23:01:02 +0100

    hero 💪

--- src/components/pages/Hero.svelte
+++ src/components/pages/Hero.svelte
@@ -5,7 +5,7 @@
 
 <div class="flex flex-col-reverse md:flex-row gap-10 md:gap-0 mt-40 ">
     <div
-        class="flex flex-col justify-between align-baseline h-1/2 mb-4 gap-8 w-4/5 md:w-3/5 m-auto md:m-0 text-center"
+        class="flex flex-col justify-between align-baseline h-1/2 mb-4 gap-8 w-4/5 md:w-3/5 m-auto md:m-0 md:text-center"
     >
         <h1
             class="text-3xl sm:text-5xl lg:text-6xl font-extrabold bg-gradient-to-r from-primary-3 via-primary-2 to-primary-1 bg-clip-text text-transparent"
